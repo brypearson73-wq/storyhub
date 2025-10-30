ab703d8b98a16e931f23369d634e5d25652195b8
brypearson73-wq <brypearson73@gmail.com>
2025-10-30 10:49:38 -0400

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Storyhub from './Storyhub_v3_00.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Storyhub />
+  </React.StrictMode>
+)
